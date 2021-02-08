03cef9cedfd3142462e3b75f731abdd400da5b8b
Killglove <sev.webmaster.k@gmail.com>
2021-02-08 15:31:44 +0100

    Added os.leave() and the basic logic for init(), update() and uninit() functions

--- main.cpp
+++ main.cpp
@@ -16,6 +16,13 @@ static int readChannel(lua_State *L){
     return 0;
 }
 
+bool lua_continue_update = true;
+static int exitUpdateLoop(lua_State *L)
+{
+	lua_continue_update=false;
+	return 0;
+}
+
 static luaL_Reg ADCLib[] = {
     {"readChannel", readChannel},
     {NULL, NULL}
@@ -26,17 +33,30 @@ void set_aux_libs(lua_State *L)
     lua_newtable(L);
     luaL_setfuncs(L, ADCLib, 0);
     lua_setglobal(L, "ADC");
+    lua_getglobal(L, "os");
+    lua_pushliteral(L, "leave");
+    lua_pushcfunction(L, exitUpdateLoop);
+    lua_settable(L, 1); 
 }
 
-int main(void){
+int main(void)
+{
     lua_State *lua = luaL_newstate(); //lua interpreter
     luaL_openlibs(lua); //loads: basic, coroutine, package, string, table, math, io, os, debug
-    set_aux_libs(lua);
+    set_aux_libs(lua); //loads: ADC
     if(luaL_dofile(lua, "main.lua")==0)//if reading the file is successful
     {
-        lua_getglobal(lua, "test"); //get the test global
+        lua_getglobal(lua, "init"); //get the test global
         lua_call(lua, 0,0); //call it with 0 arguments and 0 return values
+        while(lua_continue_update==true)
+        {
+            lua_getglobal(lua, "update"); //get the test global
+            lua_call(lua, 0,0); //call it with 0 arguments and 0 return values
+        }
+	lua_getglobal(lua, "uninit");
+	lua_call(lua,0,0);
     }
+    printf("After dofile\n");
     lua_close(lua);
     return 0;
 }
--- main.lua
+++ main.lua
@@ -1,9 +1,18 @@
-function test()
+print("file loaded")
+
+function init()
+	print("init loaded")
+end
+
+function update(dt)
 	--io.write("eee")
 	--for i,v in pairs(_ENV) do
 	--	print(i)
 	--end
-	for i=0, 7 do
-		print(ADC.readChannel(i))
-	end
+	print(ADC.readChannel(0)) --read channel 0 as it's the only connected one rn
+	--os.leave() --leaves the update loop
+end
+
+function uninit()
+	print("uninit loaded")
 end
